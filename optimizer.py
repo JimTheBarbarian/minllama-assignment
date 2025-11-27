986d28b77fa21b48644c7657f714afa3632a7b5e
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-11-27 01:00:45 -0800

    wrote AdamW

--- optimizer.py
+++ optimizer.py
@@ -38,23 +38,44 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
 
                 # State should be stored in this dictionary
+                if p not in self.state:
+                    self.state[p] = {}
+                    self.state[p]["step"] = 0
+                    # Initialize first and second moment vectors
+                    self.state[p]["exp_avg"] = torch.zeros_like(p.data)
+                    self.state[p]["exp_avg_sq"] = torch.zeros_like(p.data)
                 state = self.state[p]
-
+                m,v = state["exp_avg"], state["exp_avg_sq"]
+                state["step"] += 1
+                t = state["step"]
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
 
                 # Update first and second moments of the gradients
+                beta1, beta2 = group["betas"]
+
+                m.mul_(beta1).add_(1 - beta1, grad)
+                v.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+
 
                 # Bias correction
+                if group["correct_bias"]:
+                    m = m / (1- beta1**t)
+                    v = v / (1-beta2**t)
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
-
+                denom = v.sqrt().add_(group["eps"])
+                step_size = alpha
                 # Update parameters
+                p.data.addcdiv_(m, denom, value=-step_size)
+
 
                 # Add weight decay after the main gradient-based updates.
+                if group["weight_decay"] != 0:
+                    p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
                 # Please note that the learning rate should be incorporated into this update.
 
         return loss
